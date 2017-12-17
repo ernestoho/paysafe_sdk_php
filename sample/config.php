592b2a8d2e341f3f0ee5f6a5e0d7d525624ef204
ernestoho <junior.ho@gmail.com>
2017-12-16 22:32:46 -0400

    Update config.php

--- sample/config.php
+++ sample/config.php
@@ -1,13 +1,13 @@
 <?php
 
-    $paysafeApiKeyId = 'your-key-id';
-    $paysafeApiKeySecret = 'your-key-secret';
-    $paysafeAccountNumber = 'your-account-number';
+    $paysafeApiKeyId = 'test_ernexto';
+    $paysafeApiKeySecret = 'B-qa2-0-5a35d321-0-302d021454bfc0f0e290d1b1a49edc0028deb55dce3470c102150094375cdb1619663689c9f2958b3c0bf3155a3afb';
+    $paysafeAccountNumber = '1001177820';
 // The currencyCode should match the currency of your Paysafe account.
 // The currencyBaseUnitsMultipler should in turn match the currencyCode.
 // Since the API accepts only integer values, the currencyBaseUnitMultiplier is used convert the decimal amount into the accepted base units integer value.
-    $currencyCode = 'your-account-currency-code'; // for example: CAD
-    $currencyBaseUnitsMultiplier = 'currency-base-units-multiplier'; // for example: 100
+    $currencyCode = 'USD'; // for example: CAD
+    $currencyBaseUnitsMultiplier = '100'; // for example: 100
 
     require_once('../source/paysafe.php');
     
